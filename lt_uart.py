e69158de3c61ce9823fdd1d4c8b65cbc288467b5
Alphor KAZAMA <39847836+togalock@users.noreply.github.com>
2022-12-20 21:08:40 +0900

    Optimization by Dropping Frames

--- lt_uart.py
+++ lt_uart.py
@@ -3,7 +3,7 @@ from time import sleep
 import LT_Decode_with_Factory as LT
 
 class LT_UART:
-    def __init__(self, addr, rate, timeout = 100):
+    def __init__(self, addr, rate, timeout = 0.001):
         self.addr, self.rate = addr, rate
         self.f = serial.Serial(addr, rate, 
             timeout = timeout)
@@ -26,8 +26,8 @@ class LT_UART:
     def get_frame(self):
         return LT.poll_LTFrame(self.buffer)
 
-    def poll_frame(self, poll_first = True):
-        if poll_first: self.poll()
+    def poll_frame(self, poll_size = 50, poll_first = True):
+        if poll_first: self.poll(poll_size)
         frame = self.get_frame()
         if frame:
             self.buffer = self.buffer[frame[1]::]
--- turtle_test_LT.py
+++ turtle_test_LT.py
@@ -2,6 +2,7 @@ from math import sin, cos, atan
 import turtle as t
 from EWMF_Filter import *
 import lt_uart
+from time import sleep
 
 def rotate_m90(x, y):
     return (-y, x)
@@ -17,23 +18,33 @@ def setInterval(f, t_ms):
     def wrapper():
         nonlocal is_running
         if is_running[0]:
-            t.ontimer(f, t_ms)
+            f()
             t.ontimer(wrapper, t_ms)
             
     wrapper()
     return is_running
 
-lt_uart = lt_uart.LT_UART("/dev/ttyS0", 115200)
+ADDR = "/dev/cu.usbmodem542A0368641"
+lt_uart = lt_uart.LT_UART(ADDR, 115200, timeout = 0.0001)
 tag_radial, disappear_timeout = None, 0
 last_lt_loc, last_lt_time = None, None
+drop_frame_coolout = 0
 
 def tag_tick():
-    global tag_radial, disappear_timeout, last_frame, lt_uart
-    last_frame = lt_uart.poll_frame()
+    global tag_radial, disappear_timeout, drop_frame_coolout,\
+           last_frame, lt_uart
+    last_frame = lt_uart.poll_frame(500)
     
-    if last_frame is None:
+    if not last_frame:
         last_lt_loc = None
     else:
+        if drop_frame_coolout > 0:
+            drop_frame_coolout -= 3
+            last_lt_loc = None
+            return None
+
+        print(drop_frame_coolout)
+        drop_frame_coolout = 1
         last_lt_loc = last_frame[0].data[0][1] \
                       if last_frame and len(last_frame[0].data) \
                       else None
@@ -48,7 +59,7 @@ def tag_tick():
         return None
 
     disappear_timeout = 0
-    r, a = last_lt_loc.d * 100, last_lt_loc.a
+    r, a = last_lt_loc.d * 1000, last_lt_loc.a
     t = last_lt_time
 
     if tag_radial is None:
@@ -80,6 +91,7 @@ trend_dot.hideturtle()
 
 def draw_tag():
     global tag_radial, fresh_for
+    
     if tag_radial:
         x0, y0 = EWMF_RadialD.ra_to_xy(tag_radial.r.v, tag_radial.a.v)
         x0, y0 = rotate_m90(x0, y0)
@@ -93,10 +105,10 @@ def draw_tag():
         trend_dot.up()
         trend_dot.goto(x0, y0)
         trend_dot.down()
-        for (r, a) in tag_radial.trend_iter(2000, 10):
+        for (r, a) in tag_radial.trend_iter(2000, 3):
             x, y = EWMF_RadialD.ra_to_xy(r, a)
             trend_dot.goto(*rotate_m90(x, y))
-
+    
     else:
         tag_dot.clear()
         trend_dot.clear()
@@ -121,7 +133,7 @@ screen = t.Screen()
 t.setup(800, 600)
 t.update()
 
-setInterval(tag_tick, 100)
-setInterval(draw_tag, 100)
+setInterval(tag_tick, 20)
+setInterval(draw_tag, 50)
 
 t.mainloop()
