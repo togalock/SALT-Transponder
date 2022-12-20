809d4306c89673052a5619b499805308d73e3174
wlph17 <wlph17@gmail.com>
2022-12-20 17:23:31 +0800

    Fixes UART Polling Logic

--- lt_uart.py
+++ lt_uart.py
@@ -3,25 +3,34 @@ from time import sleep
 import LT_Decode_with_Factory as LT
 
 class LT_UART:
+    TIMEOUT_MS = 100
+
     def __init__(self, addr, rate):
         self.addr, self.rate = addr, rate
-        self.f = serial.Serial(addr, rate)
+        self.f = serial.Serial(addr, rate, 
+            timeout = self.TIMEOUT_MS)
         self.buffer = []
 
-    def poll(self):
-        return self.f.readinto(self.buffer)
-
+    def poll(self, size = 1):
+        new_data = self.f.read(size)
+        if not new_data: return None
+        
+        if len(new_data) == 1:
+            self.buffer.append(new_data)
+        else:
+            self.buffer.extend(new_data)
+        
+        return len(new_data)
+            
+    
     def get_frame(self):
-        return LT.poll_LTFrame(buffer)
+        return LT.poll_LTFrame(self.buffer)
 
     def poll_frame(self, poll_first = True):
         if poll_first: self.poll()
+        print(self.buffer)
         frame = self.get_frame()
         if frame:
             self.buffer = self.buffer[frame[1]::]
-        print(frame)
 
 lt_uart = LT_UART("/dev/ttyS0", 115200)
-
-while True:
-    print(lt_uart.poll_frame())
