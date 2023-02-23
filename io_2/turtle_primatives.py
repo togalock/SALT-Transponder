9d687c1071a6096c1a1fb703953af1f6a84da780
Alphor KAZAMA <39847836+togalock@users.noreply.github.com>
2023-02-23 23:30:49 +0900

    Included new Turtle Primatives and Helpers

--- io_2/turtle_primatives.py
+++ io_2/turtle_primatives.py
@@ -2,13 +2,65 @@ import turtle
 
 rad = lambda d: 0.0174533 * d
 deg = lambda r: 57.29578 * r
+px = lambda w, h: (w * turtle.window_width() // 2, h * turtle.window_height() // 2)
 
-def Circle(t: turtle.Turtle, x, y, r):
+COLORS = {"g": "#55FF55", "b": "#55FFFF", "r": "#FF5555",
+          "m": "#FF55FF", "y": "#FFFF55", "w": "#FFFFFF"}
+
+def setTimeout(f, t_ms):
+    turtle.ontimer(f, t_ms)
+
+def setInterval(f, t_ms):
+    is_running = [True, ]
+    def wrapper():
+        nonlocal is_running
+        if is_running[0]:
+            turtle.ontimer(f, t_ms)
+            turtle.ontimer(wrapper, t_ms)
+            
+    wrapper()
+    return is_running
+
+def fill_shape(f):
+    def wrapper(*args, **kwargs):
+        turtle.begin_fill()
+        f(*args, **kwargs)
+        turtle.end_fill()
+    
+    return wrapper()
+
+def CircleC(t: turtle.Turtle, x, y, r):
     t.up()
     t.seth(0)
     t.goto(x, y - r)
     t.down()
     t.circle(r)
+
+def ArcC(t: turtle.Turtle, x, y, r, a1, a2, draw_r = False):
+    t.up()
+    t.goto(x, y)
+    t.seth(a1)
+    if draw_r:
+        t.down()
+    t.fd(r)
+    t.left(90)
+    t.down()
+    t.circle(r, a2 - a1)
+    if draw_r:
+        t.goto(x, y)
+
+def ArrowC(t: turtle.Turtle, x, y, r, a):
+    t.up()
+    t.seth(a)
+    t.goto(x1, y1)
+    t.down()
+    t.fd(r)
+    t.left(135)
+    t.fd(r // 4)
+    t.back(r // 4)
+    t.left(45)
+    t.fd(r // 4)
+    
 
 def RectX(t: turtle.Turtle, x1, y1, x2, y2):
     t.up()
@@ -26,7 +78,20 @@ def RectC(t: turtle.Turtle, x, y, w, h):
     RectX(t, x1, y1, x2, y2)
 
 def DiamondX(t: turtle.Turtle, x1, y1, x2, y2):
-    pass
+    half_x, half_y = (x1 + x2) // 2, (y1 + y2) // 2
+    t.up()
+    t.goto(half_x, y1)
+    t.down()
+    t.goto(x2, half_y)
+    t.goto(half_x, y2)
+    t.goto(x1, half_y)
+    t.goto(half_x, y1)
+
+def DiamondC(t: turtle.Turtle, x, y, w, h):
+    half_w, half_h = w // 2, h // 2
+    x1, y1 = x - half_w, y + half_h
+    x2, y2 = x + half_w, y - half_h
+    DiamondX(t, x1, y1, x2, y2)
 
 def TriUpX(t: turtle.Turtle, x1, y1, x2, y2, p_left = 0.5):
     #  1
@@ -44,3 +109,22 @@ def TriUpC(t: turtle.Turtle, x, y, w, h, p_left = 0.5):
     x1, y1 = x - half_w, y + half_h
     x2, y2 = x + half_w, y - half_h
     TriUpX(t, x1, y1, x2, y2, p_left)
+
+def Text(t: turtle.Turtle, text, x1, y2,
+         w=None, h=None, align="center",
+         font=None, font_size=None, font_type=None,):
+    WIDTH_PER_PT, HEIGHT_PER_PT = 1.008, 1.4
+    DEFAULT_FONT, DEFAULT_SIZE, DEFAULT_TYPE = "JetBrains Mono", 10, "normal"
+    font = font or DEFAULT_FONT
+    font_size = font_size or DEFAULT_SIZE
+    font_type = font_type or DEFAULT_TYPE
+    if w:
+        font_size = min(font_size, w / len(text) / WIDTH_PER_PT)
+    if h:
+        font_size = min(font_size, h / HEIGHT_PER_PT)
+    t.up()
+    t.goto(x1, y2)
+    t.down()
+    t.write(text, align=align, font=(font, font_size, font_type))
+    return (len(text) * font_size * WIDTH_PER_PT,
+            font_size * HEIGHT_PER_PT)
