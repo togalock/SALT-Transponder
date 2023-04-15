e339e454fe4947080c63f6e717068e37e3ce9f3f
Alphor KAZAMA <39847836+togalock@users.noreply.github.com>
2023-04-16 06:46:11 +0900

    Updated Colors

--- io_2/turtle_primatives.py
+++ io_2/turtle_primatives.py
@@ -7,11 +7,16 @@ px = lambda w, h: (w * turtle.window_width() // 2, h * turtle.window_height() //
 
 # See Minecraft Color Codes:
 # [a-f] = [Green, Cyan, Red, Magenta, Yellow, White]
-COLORS = {"a": "#55FF55", "b": "#55FFFF", "c": "#FF5555",
-          "d": "#FF55FF", "e": "#FFFF55", "f": "#FFFFFF"}
-
-def c_tuple(v: complex):
-    return (v.real, v.imag)
+# [g1-g3] = Ground Shades, Lightest -> Darkest
+COLORS = {
+    "0": "#000000",
+    "1": "#0000AA", "2": "#00AA00", "3": "#00AAAA",
+    "4": "AA0000", "5": "#AA00AA", "6": "#FFAA00",
+    "7": "AAAAAA", "8": "#555555", "9": "#5555FF",
+    "a": "#55FF55", "b": "#55FFFF", "c": "#FF5555",
+    "d": "#FF55FF", "e": "#FFFF55", "f": "#FFFFFF",
+    "g1": "#CC9933", "g2": "#996600", "g3": "#660000",
+    }
 
 def setTimeout(f, t_ms):
     turtle.ontimer(f, t_ms)
