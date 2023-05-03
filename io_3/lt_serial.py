c9f3f3e56b18aa560ca652ea7ed63cf0159ab111
Alphor KAZAMA <39847836+togalock@users.noreply.github.com>
2023-05-03 17:59:16 +0900

    Added option to use dummy file

--- io_3/lt_serial.py
+++ io_3/lt_serial.py
@@ -1,5 +1,6 @@
 import serial
 import serial.tools.list_ports
+import time
 
 def get_serial_interactive():
     serial_list = serial.tools.list_ports.comports()
@@ -9,9 +10,21 @@ def get_serial_interactive():
     print("COMs Available: ")
     for i, port_info in enumerate(serial_list):
         print("[%i] %s" % (i, port_info.name))
-    
-    device = serial_list[int(input("Enter COM Index: "))].device
+        
+    use_device = input("Enter COM Index, or filename: ")
     baudrate = int(input("Baudrate [%s]: " % DEFAULT_RATE) or DEFAULT_RATE)
     timeout = float(input("Timeout [%s]: " % DEFAULT_TIMEOUT) or DEFAULT_TIMEOUT)
     
-    return serial.Serial(port=device, baudrate=baudrate, timeout=timeout)
+    if use_device.isdigit():
+        device = serial_list[int(use_device)].device
+        return serial.Serial(port=device, baudrate=baudrate, timeout=timeout)
+    else:
+        dummy_device = open(use_device, "rb")
+        
+        _read = dummy_device.read
+        def read_with_timeout(size = -1):
+            time.sleep(timeout)
+            return _read(min(baudrate, size))
+        dummy_device.read = read_with_timeout
+        
+        return dummy_device
